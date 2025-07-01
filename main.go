bfd1926a09e15d3903b8b5ecb19d436dcbd945d6
Nitesh <niteshsengar9831@gmail.com>
2025-07-02 02:17:33 +0530

    complete

--- main.go
+++ main.go
@@ -1,1 +1,39 @@
-// create diff branches for diff concepts
+package main
+
+import "fmt"
+
+type Complex struct {
+	realNo int
+	imagNo int
+}
+
+func createComplexNo(realNo int, imagNo int) *Complex {
+	c := Complex{
+		realNo: realNo,
+		imagNo: imagNo,
+	}
+	return &c
+}
+
+func (c *Complex) add(a *Complex) *Complex {
+	addImag := c.imagNo + a.imagNo
+	addReal := c.realNo + a.realNo
+	afterAdd := Complex{
+		realNo: addReal,
+		imagNo: addImag,
+	}
+	return &afterAdd
+}
+
+func (c *Complex) display() {
+	fmt.Println(c.realNo, "+", c.imagNo, "i")
+}
+
+func main() {
+	c1 := createComplexNo(1, 2)
+	c2 := createComplexNo(1, 2)
+	c1.display()
+	c2.display()
+	c3 := c1.add(c2)
+	c3.display()
+}
